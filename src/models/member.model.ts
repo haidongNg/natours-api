ef069bae727bf498b01f46824efbe088e7d31690
haidongNg <nguyenhaidong1412@gmail.com>
2021-10-16 21:38:16 +0700

    create route member and use db postgresql

---
+++ src/models/member.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Member extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+
+  constructor(data?: Partial<Member>) {
+    super(data);
+  }
+}
+
+export interface MemberRelations {
+  // describe navigational properties here
+}
+
+export type MemberWithRelations = Member & MemberRelations;
